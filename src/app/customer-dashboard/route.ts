db46a3cd0b1048c2da6dd8b87e924557a7716eee
parth2892 <parth28488@gmail.com>
2018-05-27 22:29:25 +0530

    Commit for add cutomer detail page

--- src/app/customer-dashboard/route.ts
+++ src/app/customer-dashboard/route.ts
@@ -6,16 +6,11 @@ export const route: Routes = [
         path: '',
         component: CustomerDashboardComponent,
         children: [
-            /*{
-                path: 'add-new',
-                canActivate: [AuthGuard],
-                loadChildren: 'src/app/dashboard/manage-service/add-new/module#AddNewModule'
-            },
             {
-                path: 'view-booking',
-                canActivate: [AuthGuard],
-                loadChildren: 'src/app/dashboard/manage-service/view-booking/module#ViewBookingModule'
-            }*/
+                path: 'add-customer-detail',
+                //canActivate: [AuthGuard],
+                loadChildren: 'src/app/customer-dashboard/manage-customer/add-customer-detail/module#AddCustomerDetailModule'
+            }
         ]
     }
 ];
