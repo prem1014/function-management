d9f1556e2e265132022337287e8082fdb71f2c27
parth2892 <parth28488@gmail.com>
2018-06-02 03:34:53 +0530

    Booking buttona and design modification for serach

--- src/app/_widgets/search-result/component.ts
+++ src/app/_widgets/search-result/component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-search-result-list',
@@ -11,12 +12,23 @@ export class SearchResultComponent implements OnInit {
     @Input() arrResult: any = [];
     public resultArr: any = [];
 
-    constructor() {
+    constructor(private router: Router) {
         this.resultArr = [];
         this.arrResult = [];
     }
 
     ngOnInit() {
         this.resultArr = this.arrResult;
+    }
+
+    public bookNow(bookId) {
+        console.log(bookId);
+        sessionStorage.setItem('serviceId', bookId);
+
+        if( sessionStorage.getItem('isLoggedIn') == "true" ) {
+            this.router.navigateByUrl('/booking');
+        } else {
+            this.router.navigateByUrl('/login');
+        }
     }
 }
